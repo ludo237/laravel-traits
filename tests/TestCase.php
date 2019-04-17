f8c67212fd0e4b83204bd14770d8556792d529e3
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2019-04-17 17:51:09 +0200

    Add weird test on TestCase to avoid warnings
    This needs to be fixed later...

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,8 @@
 namespace Ludo237\EloquentTraits\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 /**
  * TestCase
@@ -15,7 +17,27 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function setUp() : void
     {
         parent::setUp();
-    
+        
         $this->loadMigrationsFrom(__DIR__ . "/database/migrations/");
     }
+    
+    
+    /** @test */
+    public function it_runs_the_migrations()
+    {
+        $user = DB::table("users")->where("id", "=", 1)->first();
+        
+        $this->assertNull($user);
+        
+        $columns = Schema::getColumnListing("users");
+        $this->assertEquals([
+            "id",
+            "uuid",
+            "slug",
+            "name",
+            "created_at",
+            "updated_at",
+        ], $columns);
+    }
+    
 }
